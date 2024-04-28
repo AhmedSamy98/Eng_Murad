dd5e8dba8b815d6d76f35be4a75ddd090a48a7e8
AhmedSamy98 <ahmed1samy1998@gmail.com>
2024-04-28 12:17:49 +0300

    constant

---
+++ Task1_RestApi/flutter_restapi/lib/constant/linkapi.dart
@@ -0,0 +1,5 @@
+// const String linkServerName = 'http://10.0.2.2/flutter_restapi_php'; // this for emulator
+const String linkServerName = 'http://192.168.1.10/flutter_restapi_php'; // this for real device { run ipconfig in cmd to get your ip address }
+//Auth
+const String linkSignUp = '$linkServerName/auth/signup.php';
+const String linkLogIn = '$linkServerName/auth/login.php';
