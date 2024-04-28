5019b85386fbe8636dae6d0d9b4c1d9b1202a6bb
AhmedSamy98 <ahmed1samy1998@gmail.com>
2024-04-28 12:07:01 +0300

    delete {TodoListApp}

--- Task1_RestApi/flutter_restapi/lib/api/api.dart
+++
@@ -1,9 +0,0 @@
-import 'package:http/http.dart' as http;
-
-class Api {
-  static Future getTodos() {
-    return http.get(
-      Uri.parse("https://jsonplaceholder.typicode.com/todos"),
-    );
-  }
-}
